ae9fc6018f802171bf3aaee24e1ae5cbccdc3638
Gerald Ke <gerald.nke@gmail.com>
2013-06-29 20:59:14 +0800

    Install some packages on fresh install

---
+++ bootstrap-linux.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt-get install vim
+sudo apt-get install xmonad suckless-tools
+sudo apt-get install google-chrome-stable
+sudo apt-get install thunar
+sudo apt-get install git
+sudo apt-get install xmobar
+sudo apt-get install terminator
+
+# configure git
+# setup github ssh keys
+# configure xmonad
+# configure bashrc
